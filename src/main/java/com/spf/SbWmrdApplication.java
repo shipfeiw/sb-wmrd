a2486c0dbedcd4d03c7d7a9f38688c7a594ee716
shipengfei <527615754@qq.com>
2018-05-03 11:15:23 +0800

    init project

---
+++ src/main/java/com/spf/SbWmrdApplication.java
@@ -0,0 +1,12 @@
+package com.spf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbWmrdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbWmrdApplication.class, args);
+    }
+}
